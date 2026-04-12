7427172865e7980e8e814935c87e67f5f93f3499
Felipe Carvalho <felipe_pc7@hotmail.com>
2026-04-12 15:39:39 -0300

    feat: project setup, docker config and base structure

---
+++ src/main/java/com/pinhadev/socketalk/SocketalkApplication.java
@@ -0,0 +1,13 @@
+package com.pinhadev.socketalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinhadev/socketalk/SocketalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinhadev.socketalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
